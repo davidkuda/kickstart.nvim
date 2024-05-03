8b69d48b46b2bb0a720c0e2a1d0d2edc00b50f08
David Kuda <david.kuda@noser.com>
2024-05-03 08:12:26 +0200

    put my plugins into custom/plugins/init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'Pocco81/auto-save.nvim',
+    config = function()
+      require('auto-save').setup {
+        -- your config goes here
+        -- or just leave it empty :)
+      }
+    end,
+  },
+  {
+    'azabiong/vim-highlighter',
+    init = function()
+      -- settings
+    end,
+  },
+  { 'akinsho/git-conflict.nvim', version = '*', config = true },
+}
--- lua/lazy_plugins.lua
+++ lua/lazy_plugins.lua
@@ -648,22 +648,6 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
-  {
-    "Pocco81/auto-save.nvim",
-    config = function()
-      require("auto-save").setup {
-        -- your config goes here
-        -- or just leave it empty :)
-      }
-    end,
-  },
-  {
-    "azabiong/vim-highlighter",
-    init = function()
-      -- settings
-    end,
-  },
-  { 'akinsho/git-conflict.nvim', version = "*", config = true },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -683,7 +667,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
