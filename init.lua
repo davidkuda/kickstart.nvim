ffc2abae9e607b41ef2ecf79b3f8188d092c0206
David Kuda <david.kuda@noser.com>
2024-05-03 08:10:47 +0200

    rm modeline cmt

--- init.lua
+++ init.lua
@@ -3,8 +3,3 @@ require("keymaps")
 require("autocmds")
 require("lazy_plugins")
 require("custom")
-
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
-
