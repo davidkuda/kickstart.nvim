6fe0c207b8653fecbac7f60c105321af88df1078
David Kuda <david.kuda@noser.com>
2024-05-28 16:58:48 +0200

    copy to clipboard in v with Y

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -29,3 +29,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+
+-- David:
+-- copy to clipboard
+vim.keymap.set('v', 'Y', '"+y')
